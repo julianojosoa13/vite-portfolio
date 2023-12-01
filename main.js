fe0764808331df209e9a54a29bbcd0735da2cff2
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2023-12-01 10:56:28 +0300

    Added animation to a 3D Object

--- main.js
+++ main.js
@@ -17,3 +17,23 @@ renderer.setSize( window.innerWidth, window.innerHeight)
 camera.position.setZ(30)
 
 renderer.render(scene, camera)
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100)
+
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347, wireframe: false } )
+
+const torus = new THREE.Mesh( geometry, material );
+
+scene.add(torus)
+
+function animate() {
+  requestAnimationFrame( animate );
+
+  torus.rotation.x += 0.01
+  torus.rotation.y += 0.005
+  torus.rotation.z += 0.01
+
+  renderer.render( scene, camera )
+}
+
+animate()

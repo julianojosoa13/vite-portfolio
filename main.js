a01f0b2405c2810cac49b98b15385818dfca6fe7
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2023-12-01 11:40:04 +0300

    AddStar function

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './style.css'
 
 import * as THREE from 'three'
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -39,7 +41,23 @@ const lightHelper = new THREE.PointLightHelper(pointLight)
 
 const gridHelper = new THREE.GridHelper(200,50)
 
-scene.add(lightHelper, gridHelper)
+// scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+const addStart = () => {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24)
+
+  const material = new THREE.MeshStandardMaterial( {color: 0xffffff} )
+
+  const star = new THREE.Mesh(geometry, material)
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z)
+  
+  scene.add(start)
+}
 
 
 function animate() {
@@ -48,6 +66,8 @@ function animate() {
   torus.rotation.x += 0.01
   torus.rotation.y += 0.005
   torus.rotation.z += 0.01
+
+  controls.update()
 
   renderer.render( scene, camera )
 }
